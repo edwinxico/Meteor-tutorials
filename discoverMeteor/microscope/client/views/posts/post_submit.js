293d0b9a3264e7263b15c28701c5eecb77b31811
XicoOfficial <xicoofficial@gmail.com>
2015-11-06 22:03:01 -0600

    Back before the demostration of the order htat posts appear

--- discoverMeteor/microscope/client/views/posts/post_submit.js
+++ discoverMeteor/microscope/client/views/posts/post_submit.js
@@ -15,8 +15,8 @@ Template.postSubmit.events({
       if (result.postExists)
       	alert('This link has already been posted');
 
-    });
+      Router.go('postPage', {_id: result._id});
 
-		Router.go('postsList');  
+    });  
 	}
 });
