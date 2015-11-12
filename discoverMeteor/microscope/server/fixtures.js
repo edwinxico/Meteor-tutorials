4b5d4cf8db7f406ae67267e2d02414bb0949a1ec
XicoOfficial <xicoofficial@gmail.com>
2015-11-11 21:17:25 -0600

    Denormalized the number of comments into the post.

--- discoverMeteor/microscope/server/fixtures.js
+++ discoverMeteor/microscope/server/fixtures.js
@@ -16,7 +16,8 @@ if (Posts.find().count() === 0) {
 		userId: sacha._id,
 		author: sacha.profile.name,
 		url: 'http://sachagreif.com/introducing-telescope/',
-		submitted: now - 7 * 3600 * 1000
+		submitted: now - 7 * 3600 * 1000,
+		commentsCount: 2
 	});
 	
 	Comments.insert({
@@ -40,14 +41,16 @@ if (Posts.find().count() === 0) {
 		userId: tom._id,
 		author: tom.profile.name,
 		url: 'http://meteor.com',
-		submitted: now - 10 * 3600 * 1000
+		submitted: now - 10 * 3600 * 1000,
+		commentsCount: 0
 	});
-	
+
 	Posts.insert({
 		title: 'The Meteor Book',
 		userId: tom._id,
 		author: tom.profile.name,
 		url: 'http://themeteorbook.com',
-		submitted: now - 12 * 3600 * 1000
+		submitted: now - 12 * 3600 * 1000,
+		commentsCount: 0
 	});
 }
