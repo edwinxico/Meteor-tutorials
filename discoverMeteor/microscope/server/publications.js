882591dee224bf4f30609e13cfaafad1ea12f60e
XicoOfficial <xicoofficial@gmail.com>
2015-11-12 18:21:00 -0600

    Added basic notifications collection.+

--- discoverMeteor/microscope/server/publications.js
+++ discoverMeteor/microscope/server/publications.js
@@ -5,4 +5,8 @@ Meteor.publish('posts', function() {
 Meteor.publish('comments', function(postId) {
 	check(postId, String);
 	return Comments.find({postId: postId});
-})
+});
+
+Meteor.publish('notifications', function() {
+	return Notifications.find();
+});
