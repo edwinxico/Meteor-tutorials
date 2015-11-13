f6bfec11d31b2e158dfd4510c27fb82104bf29e6
XicoOfficial <xicoofficial@gmail.com>
2015-11-12 20:02:20 -0600

    Refactored postsLists route into a RouteController.

--- discoverMeteor/microscope/lib/router.js
+++ discoverMeteor/microscope/lib/router.js
@@ -6,6 +6,31 @@ Router.configure({
 	}
 });
 
+PostsListController = RouteController.extend({
+	template: 'postsList',
+	increment: 5,
+	postsLimit: function() {
+		return parseInt(this.params.postsLimit) || this.increment;
+	},
+	findOptions: function() {
+		return {sort: {submitted: -1}, limit: this.postsLimit()};
+	},
+	waitOn: function() {
+		return Meteor.subscribe('posts', this.findOptions());
+	},
+	posts: function() {
+		return Posts.find({}, this.findOptions());
+	},
+	data: function() {
+		var hasMore = this.posts().count() === this.postsLimit();
+		var nextPath = this.route.path({postsLimit: this.postsLimit() + this.increment});
+		return {
+			posts: this.posts(),
+			nextPath: hasMore ? nextPath : null
+		};
+	}
+});
+
 Router.route('/posts/:_id', {
 	name: 'postPage',
 	waitOn: function() {
@@ -24,17 +49,7 @@ Router.route('/submit', {
 });
 
 Router.route('/:postsLimit?', {
-	name: 'postsList',
-	waitOn: function() {
-		var limit = parseInt(this.params.postsLimit) || 5;
-		return Meteor.subscribe('posts', {sort: {submitted: -1}, limit: limit});
-	},
-	data: function() {
-		var limit = parseInt(this.params.postsLimit) || 5;
-		return {
-			posts: Posts.find({}, {sort: {submitted: -1}, limit: limit})
-		};
-	}
+	name: 'postsList'
 });
 
 var requireLogin = function(){
