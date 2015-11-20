0d222573532de4b29df16cc025b7326262022511
XicoOfficial <xicoofficial@gmail.com>
2015-11-19 19:30:39 -0600

    Added active classes to the header.

---
+++ discoverMeteor/microscope/client/views/includes/header.js
@@ -0,0 +1,12 @@
+Template.header.helpers({
+	activeRouterClass: function(/* route names */) {
+		var args = Array.prototype.slice.call(arguments, 0);
+		args.pop();
+
+		var active = _.any(args, function(name) {
+			return Router.current() && Router.current().route.getName() == name
+		});
+
+		return active && 'active';
+	}
+});
